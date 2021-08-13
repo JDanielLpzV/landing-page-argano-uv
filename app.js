5b568d5852f1fc28b6c959b31e094180d08addaf
misa <migocr@gmail.com>
2021-08-13 11:19:46 -0600

    Modal button style, comments added to code.

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 window.onscroll = function() {menuSticky()};
+// Menu on scroll sticky funcion
 function menuSticky() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         console.log("scroll");
       document.getElementById("menuNav").style = "transition: 0.4s; padding: .5em 8%; font-size: .9em;";
-      //document.getElementById("logo").style.fontSize = "25px";
     } else {
         console.log("scroll");
       document.getElementById("menuNav").style = "transition: 0.4s; height: auto; font-size:1em; ";
-      //document.getElementById("logo").style.fontSize = "35px";
     }
 }
 
