b1fe6fc9e83b388c90dc6ef0bced45fcc6a63c39
misa <migocr@gmail.com>
2021-08-13 15:15:52 -0600

    button up to top added

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ function menuSticky() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         console.log("scroll");
       document.getElementById("menuNav").style = "transition: 0.4s; padding: .5em 8%; font-size: .9em;";
+      document.getElementById("back-to-top").style.display= "block";
+      
     } else {
         console.log("scroll");
       document.getElementById("menuNav").style = "transition: 0.4s; height: auto; font-size:1em; ";
+      document.getElementById("back-to-top").style.display= "none";
     }
 }
 
